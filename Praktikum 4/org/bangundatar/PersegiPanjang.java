240810c9dfec9aa499c4fc4c6e7f87c0c63c7e71
dinal <101868747+AhlisDinalBahtiar@users.noreply.github.com>
2024-03-16 23:07:39 +0700

    Pengumpulan Tugas 4

---
+++ Praktikum 4/org/bangundatar/PersegiPanjang.java
@@ -0,0 +1,21 @@
+package org.bangundatar;
+
+import org.poligon.Poligon;
+
+public class PersegiPanjang extends Poligon{
+    private int panjang, lebar;
+
+    public PersegiPanjang(int panjang, int lebar, int jumlahSisi){
+        this.panjang = panjang;
+        this.lebar = lebar;
+        this.jumlahSisi = jumlahSisi;
+    }
+
+    public double hitungLuas(){
+        return panjang * lebar;
+    }
+
+    public void printInfo(){
+        System.out.println("Bangun Persegi Panjang bersisi " + this.getJumlahSisi());
+    }
+}
